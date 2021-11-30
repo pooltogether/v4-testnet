a0c34f0790855cad4430d216f32565b2756b623c
Kames Geraghty <kamescg@gmail.com>
2021-11-30 15:20:55 +0000

    update(configureCoreDeployment): add DrawCalculatorTimelock to configuration steps

--- helpers/configureCoreDeployment.ts
+++ helpers/configureCoreDeployment.ts
@@ -1,25 +1,37 @@
 import { cyan, green } from "chalk"
 
 export async function configureCoreDeployment(ethers: any, manager: string) {
-  const prizeDistributionBuffer = await ethers.getContract('PrizeDistributionBuffer')
-  const prizeDistributionFactory = await ethers.getContract('PrizeDistributionFactory')
   const drawAndPrizeDistributionTimelock = await ethers.getContract('DrawAndPrizeDistributionTimelock')
+  const drawCalculatorTimelock = await ethers.getContract('DrawCalculatorTimelock')
+  const prizeDistributionFactory = await ethers.getContract('PrizeDistributionFactory')
+  const prizeDistributionBuffer = await ethers.getContract('PrizeDistributionBuffer')
 
   /**
    * Management Hierarchy
    * --------------------
-   * Defender Autotask                (Manager => EOA)
-   * DrawAndPrizeDistributionTimelock (Manager => Defender Autotask)
-   * PrizeDistributionFactory         (Manager => DrawAndPrizeDistributionTimelock)
-   * PrizeDistributionBuffer          (Manager => PrizeDistributionFactory)
+   * Defender Autotask                    (Manager => EOA)
+   * DrawAndPrizeDistributionTimelock     (Manager => Defender Autotask)
+   *   DrawCalculatorTimelock             (Manager => DrawAndPrizeDistributionTimelock)
+   *   PrizeDistributionFactory           (Manager => DrawAndPrizeDistributionTimelock)
+   *     PrizeDistributionBuffer          (Manager => PrizeDistributionFactory)
    */
 
   /**
    * @dev The DrawAndPrizeDistributionTimelock contract will be managed by a Defender Autotask
    */
   if (await drawAndPrizeDistributionTimelock.manager() != manager) {
-    cyan(`\nSetting L1TimelockTrigger manager to ${manager}...`)
+    cyan(`\nSetting DrawAndPrizeDistributionTimelock manager to ${manager}...`)
     const tx = await drawAndPrizeDistributionTimelock.setManager(manager)
+    await tx.wait(1)
+    green('Done!')
+  }
+
+  /**
+   * @dev The DrawCalculatorTimelock contract will be managed by DrawAndPrizeDistributionTimelock
+   */
+  if (await drawCalculatorTimelock.manager() != drawAndPrizeDistributionTimelock.address) {
+    cyan(`\nSetting DrawCalculatorTimelock manager to ${drawAndPrizeDistributionTimelock.address}`)
+    const tx = await drawCalculatorTimelock.setManager(drawAndPrizeDistributionTimelock.address)
     await tx.wait(1)
     green('Done!')
   }
@@ -28,7 +40,7 @@ export async function configureCoreDeployment(ethers: any, manager: string) {
    * @dev The PrizeDistributionFactory contract will be managed by DrawAndPrizeDistributionTimelock
    */
   if (await prizeDistributionFactory.manager() != drawAndPrizeDistributionTimelock.address) {
-    cyan('\nSetting DrawCalculatorTimelock manager...')
+    cyan(`\nSetting PrizeDistributionFactory manager to ${drawAndPrizeDistributionTimelock.address}`)
     const tx = await prizeDistributionFactory.setManager(drawAndPrizeDistributionTimelock.address)
     await tx.wait(1)
     green('Done!')
@@ -38,7 +50,7 @@ export async function configureCoreDeployment(ethers: any, manager: string) {
    * @dev The PrizeDistributionBuffer contract will be managed by PrizeDistributionFactory
    */
   if (await prizeDistributionBuffer.manager() != prizeDistributionFactory.address) {
-    cyan('\nSetting PrizeDistributionBuffer manager...')
+    cyan(`\nSetting PrizeDistributionBuffer manager to ${prizeDistributionFactory.address}`)
     const tx = await prizeDistributionBuffer.setManager(prizeDistributionFactory.address)
     await tx.wait(1)
     green('Done!')
