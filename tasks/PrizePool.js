9e54deda4ba776bb75c1b70856527d53532fff5a
Pierrick Turelier <pierrick@turelier.com>
2021-12-08 11:13:30 -0600

    fix(tasks): fix PrizePool task

--- tasks/PrizePool.js
+++ tasks/PrizePool.js
@@ -10,26 +10,30 @@ task("deposit", "Deposits into the pool")
 .setAction(async (args, { ethers }) => {
     const { user, wallet } = await getUserAndWallet(ethers, args)
     debug(user, wallet)
-  
+
     const yieldSource = await ethers.getContract('MockYieldSource')
     const prizePool = await ethers.getContract('YieldSourcePrizePool')
-    const token = await ethers.getContractAt('ERC20Mintable', await yieldSource.depositToken())
-    const decimals = await token.decimals()
+    const token = await ethers.getContractAt(
+      '@pooltogether/v4-core/contracts/test/ERC20Mintable.sol:ERC20Mintable',
+      await yieldSource.depositToken(),
+    )
 
+    const decimals = await token.decimals()
     const balance = await token.balanceOf(wallet.address)
-    if (!balance.gte(ethers.utils.parseUnit(args.amount, decimals))) {
+
+    if (!balance.gte(ethers.utils.parseUnits(args.amount, decimals))) {
       debug(`Insufficient balance; minting...`)
-      const txMint = await token.mint(wallet.address, ethers.utils.parseUnit(args.amount, decimals))
+      const txMint = await token.mint(wallet.address, ethers.utils.parseUnits(args.amount, decimals))
       await txMint.wait()
     }
 
     debug(`Approving...`)
-    const txApprove = await token.connect(wallet).approve(prizePool.address, ethers.utils.parseUnit(args.amount, decimals))
+    const txApprove = await token.connect(wallet).approve(prizePool.address, ethers.utils.parseUnits(args.amount, decimals))
     console.log(cyan(emoji.find('🕰️').emoji, ` Approving Deposit to PrizePool`))
     await txApprove.wait()
 
     debug(`Depositing...`)
-    await prizePool.connect(wallet).depositTo(wallet.address, ethers.utils.parseUnit(args.amount, decimals))
+    await prizePool.connect(wallet).depositTo(wallet.address, ethers.utils.parseUnits(args.amount, decimals))
 
     console.log(cyan(emoji.find('✅').emoji, `Deposited ${args.amount} tokens and received ${args.amount} tickets`))
   });
