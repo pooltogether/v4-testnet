4470f4cba540010106d1848389e9c3c2e370470c
Pierrick Turelier <pierrick@turelier.com>
2022-11-28 18:54:24 -0600

    fix(dependencies): fix hardhat deps to verify contracts

--- hardhat.config.dependencies.ts
+++ hardhat.config.dependencies.ts
@@ -20,7 +20,17 @@ export const external = {
 export const dependencyCompiler = {
   paths: [
     // Core
+    '@pooltogether/v4-core/contracts/DrawBeacon.sol',
+    '@pooltogether/v4-core/contracts/DrawCalculator.sol',
+    '@pooltogether/v4-core/contracts/DrawBuffer.sol',
+    '@pooltogether/v4-core/contracts/PrizeDistributor.sol',
+    '@pooltogether/v4-core/contracts/PrizeDistributionBuffer.sol',
+    '@pooltogether/v4-core/contracts/Ticket.sol',
+    '@pooltogether/v4-core/contracts/prize-strategy/PrizeSplitStrategy.sol',
+    '@pooltogether/v4-core/contracts/Reserve.sol',
+    '@pooltogether/v4-core/contracts/prize-pool/YieldSourcePrizePool.sol',
     '@pooltogether/v4-core/contracts/test/ERC20Mintable.sol',
+    '@pooltogether/v4-core/contracts/permit/EIP2612PermitAndDeposit.sol',
     // Timelock
     '@pooltogether/v4-timelocks/contracts/L1TimelockTrigger.sol',
     '@pooltogether/v4-timelocks/contracts/L2TimelockTrigger.sol',
